d7dba5e0665296af42ee4440b134c8e24dcbfab1
yuqingwang15 <863623756@qq.com>
2017-09-24 05:21:13 -0500

    Update p02.scala

--- p02.scala
+++ p02.scala
@@ -5,70 +5,57 @@
  * @return   {[type]}                 [description]
  */
 
-class Point( val _x:Int =2,var y:Int=1 ){// 构造时写var与不写var区别？无var则不成为Point对象成员值
-	//Parameters without val or var are private values, visible only within the class.
+class Point( val _x:Int =2,var y:Int=1 ){ //Parameters without val or var are private values, visible only within the class.
 	override def  toString:String = {
 		s"($x,$y)"
 	}
 	def x : Any= _x
 }
 
-class User { val x: Long = 987654321 } 
-
-//def User//error
- 
-// import scala.reflect.ClassTag
-// def func[Any](v: Any) = v match {
-//   			case _: Int    => "Int"
-//   			case _: String => "String"
-//   			case _         => "Unknown"
-// }
+class User { val x: Long = 987654321 }  //no parameters
 
 // def f[Any](v: Any)(implicit ev: ClassTag[Any]) = ev.toString
-
-class Point2{
-	 private var _x = 0
-  private var _y = 0
-  private val bound = 100
-
-  def x = _x
-  def x_= (newValue: Int): Unit = {
-    if (newValue < bound) _x = newValue else printWarning
-  }
-
-  def y = _y
-  def y_= (newValue: Int): Unit = {
-    if (newValue < bound) _y = newValue else printWarning
-  }
-
-  private def printWarning = println("WARNING: Out of bounds")
-}
-//私有数据在_x 有x方法来取数 
 /**
  * Notice the special syntax for the setters: 
  * the method has _= appended to the identifier of the getter and the parameters come after.
  */
+class Point2{  //no parameters
+	private var _x = 0
+	private var _y = 0
+  	private val bound = 100
+	
+	def x = _x  //get the private member of class ,x is indentifier of the getter
+  	def x_= (newValue: Int): Unit = {
+		if (newValue < bound) _x = newValue else printWarning
+	}
+
+  	def y = _y
+  	def y_= (newValue: Int): Unit = {
+    		if (newValue < bound) _y = newValue else printWarning
+  	}
+
+  	private def printWarning = println("WARNING: Out of bounds")
+}
 
 object test{
 	def main(args:Array[String]){
-		
 		val x: Long = 987654321
 		val y: Float = x  // 9.8765434E8 (note that some precision is lost in this case)
 
 		val face: Char = '☺'
 		val number: Int = face  // 9786
-		val origin :Point =new Point //必须传入相同格式的参数，除非class构造时已有默认值
+		val origin :Point =new Point //必须传入相同格式的参数，除非class构造时已有默认值 new用法
 		val a :Point=new Point(face)
 		println(a.x);	println(a.y)
 
-		val point1 = new Point2 //无问题
+		val point1 = new Point2 
 		point1.x = 99
-		point1.y = 101 // prints the warning
+		point1.y = 101 //use indentifier of getter to set
 		val usr1 = new User
-		
+		//point1._x
+		//point1._y  // error: cannot be accessed
 		println(origin)
 		//println(usr1)
-		//func(4)
 	}
 }
 
