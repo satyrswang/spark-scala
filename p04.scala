e5c3603257e631b334d70a0df9a9bb7c2157f54b
yuqingwang15 <863623756@qq.com>
2017-09-24 06:29:38 -0500

    Update p04.scala

--- p04.scala
+++ p04.scala
@@ -4,34 +4,10 @@
  * @DateTime 2017-09-24T07:48:08+0800
  * @return   {[type]}                 [description]
  */
-object test{
-	def main(args:Array[String]){
-		val s   = new si("woshi")
-		s.next
-
-		//这里如果不是args(0)该如何表示从外部传入参数？则无法表示
-		class mi(val dd :String) extends si( args(0)) with Ri{
-			val m =new mi(s)
-			m foreach println
-
-}
-
-		val n = new mi(s.s_)
-
-		// val textFile = "hello wang /n yuqing yu /n qing he wang qing/n hi"
-		// val counts = textFile.flatMap(line => line.split(" "))
-  //                .map(word => (word, 1))
-  //                .reduceByKey(_ + _)
-  //       println(counts)
-
-	}
-
-}
-
 abstract class AbsIterator {
-  type T
-  def hasNext: Boolean
-  def next(): T
+	type T
+  	def hasNext: Boolean
+  	def next(): T
 }//声明
 
 trait Ri extends AbsIterator{
@@ -43,12 +19,25 @@ class si(val s:String) extends AbsIterator{
 	def s_ = s
 	type T = Char
 	private var i = 0
-  def hasNext = i < s.length //just a Boolean
-  def next() = {
-    val ch = s charAt i //s.charAt(i)
-    i += 1
-    println(ch)
-    ch //return T
+  	def hasNext = i < s.length //just a Boolean
+  	def next() = {
+    		val ch = s charAt i //s.charAt(i)
+    		i += 1
+   		 println(ch)
+   		 ch //return T
+	}
+}
+
+object test{
+	def main(args:Array[String]){
+		val s   = new si("woshi")
+		s.next
+		class mi(val dd :String) extends si( args(0)) with Ri{
+			val m =new mi(s)
+			m foreach println
+		}
+
+		val n = new mi(s.s_)
 	}
 }
 
